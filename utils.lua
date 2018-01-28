e9b1bf39202027d38e636ced6cd0807cdf88b9d1
MultiPain <32590972+MultiPain@users.noreply.github.com>
2018-01-28 17:32:38 +0900

    Update utils.lua

--- utils.lua
+++ utils.lua
@@ -1,18 +1,10 @@
-function split(str, pat)
-   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
-   local fpat = "(.-)" .. pat
-   local last_end = 1
-   local s, e, cap = str:find(fpat, 1)
-   while s do
-      if s ~= 1 or cap ~= "" then
-         table.insert(t,cap)
-      end
-      last_end = e+1
-      s, e, cap = str:find(fpat, last_end)
-   end
-   if last_end <= #str then
-      cap = str:sub(last_end)
-      table.insert(t, cap)
-   end
-   return t
+function split(str)
+	local t = {}
+	local i = 1
+	for v in string.gmatch(str, "[%-%w.]+") do 
+		local n = tonumber(v)
+		t[i] = v--(n == nil) and v or n
+		i += 1
+	end
+	return t
 end
